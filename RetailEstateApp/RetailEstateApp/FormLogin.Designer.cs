7b39a70f3ef1b2474a77108a48e214eca79ee882
Pratik Adhikari <211991@students.aih.nsw.edu.au>
2022-10-07 20:27:36 +1100

    Application commit

---
+++ RetailEstateApp/RetailEstateApp/FormLogin.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace RetailEstateApp
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.usernameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.usernameTextBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.usernameTextBox.Location = new System.Drawing.Point(732, 257);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(302, 32);
+            this.usernameTextBox.TabIndex = 0;
+            this.usernameTextBox.TextChanged += new System.EventHandler(this.usernameTextBox_TextChanged);
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.passwordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.passwordTextBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.passwordTextBox.Location = new System.Drawing.Point(732, 323);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(302, 32);
+            this.passwordTextBox.TabIndex = 1;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.loginButton.Location = new System.Drawing.Point(792, 403);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(87, 42);
+            this.loginButton.TabIndex = 2;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.usernameLabel.Location = new System.Drawing.Point(518, 257);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(113, 26);
+            this.usernameLabel.TabIndex = 3;
+            this.usernameLabel.Text = "Username";
+            this.usernameLabel.Click += new System.EventHandler(this.usernameLabel_Click);
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.passwordLabel.Location = new System.Drawing.Point(518, 323);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(108, 26);
+            this.passwordLabel.TabIndex = 4;
+            this.passwordLabel.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(776, 485);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Forgot Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1984, 872);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Name = "Form1";
+            this.Text = "REHE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ RetailEstateApp/RetailEstateApp/FormSearch.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace RetailEstateApp
+{
+    partial class FormSearch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchPropertyLabel = new System.Windows.Forms.Label();
+            this.propertySearchTextBox = new System.Windows.Forms.TextBox();
+            this.propertSearchButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // searchPropertyLabel
+            // 
+            this.searchPropertyLabel.AutoSize = true;
+            this.searchPropertyLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.searchPropertyLabel.Location = new System.Drawing.Point(462, 133);
+            this.searchPropertyLabel.Name = "searchPropertyLabel";
+            this.searchPropertyLabel.Size = new System.Drawing.Size(309, 46);
+            this.searchPropertyLabel.TabIndex = 1;
+            this.searchPropertyLabel.Text = "Search Property";
+            this.searchPropertyLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // propertySearchTextBox
+            // 
+            this.propertySearchTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.propertySearchTextBox.Location = new System.Drawing.Point(451, 230);
+            this.propertySearchTextBox.Name = "propertySearchTextBox";
+            this.propertySearchTextBox.Size = new System.Drawing.Size(320, 32);
+            this.propertySearchTextBox.TabIndex = 2;
+            // 
+            // propertSearchButton
+            // 
+            this.propertSearchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.propertSearchButton.Location = new System.Drawing.Point(800, 230);
+            this.propertSearchButton.Name = "propertSearchButton";
+            this.propertSearchButton.Size = new System.Drawing.Size(81, 32);
+            this.propertSearchButton.TabIndex = 3;
+            this.propertSearchButton.Text = "Search";
+            this.propertSearchButton.UseVisualStyleBackColor = true;
+            // 
+            // FormSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1371, 694);
+            this.Controls.Add(this.propertSearchButton);
+            this.Controls.Add(this.propertySearchTextBox);
+            this.Controls.Add(this.searchPropertyLabel);
+            this.Name = "FormSearch";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "REHE- Search";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label searchPropertyLabel;
+        private System.Windows.Forms.TextBox propertySearchTextBox;
+        private System.Windows.Forms.Button propertSearchButton;
+    }
+}
