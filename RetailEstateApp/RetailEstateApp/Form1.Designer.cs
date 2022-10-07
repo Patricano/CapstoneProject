0f5484ba2c35b9b9b9e1f44f2a72c1cc59091418
Pratik Adhikari <211991@students.aih.nsw.edu.au>
2022-10-07 21:04:41 +1100

    Property View Form Created
    In this commit a new form named 'FormPropertyView' is created. Supposed use of this form is to display information of the property along with the estimation of the value of the property.

---
+++ RetailEstateApp/RetailEstateApp/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace RetailEstateApp
+{
+    partial class FormPropertyView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelProperty = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelProperty
+            // 
+            this.labelProperty.AutoSize = true;
+            this.labelProperty.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.labelProperty.Location = new System.Drawing.Point(12, 9);
+            this.labelProperty.Name = "labelProperty";
+            this.labelProperty.Size = new System.Drawing.Size(145, 39);
+            this.labelProperty.TabIndex = 1;
+            this.labelProperty.Text = "Property";
+            // 
+            // FormPropertyView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1456, 898);
+            this.Controls.Add(this.labelProperty);
+            this.Name = "FormPropertyView";
+            this.Text = "REHE - Property";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelProperty;
+    }
+}
